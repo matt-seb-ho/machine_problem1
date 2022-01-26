5908afa34fc3d3b03dffce83c5c81ff4b9dd3087
Matthew Ho <msho@ucsb.edu>
2022-01-25 20:14:10 -0800

    79pct

--- nb_classifier.py
+++ nb_classifier.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 from time import perf_counter
+import sys
 
 # constant for normpdf
 root2pi = np.sqrt(2 * np.pi)
@@ -17,10 +18,9 @@ class NBClassifier:
 		self.gender_llh = [{}, {}]
 
 		# label_counts 
-		self.label_counts = [None, None]
-		self.train_size = None
+		# self.label_counts = [None, None]
 
-	def train(self, df):
+	def train(self, df, print_time=False):
 		# for each label, gather continuous distribution params and discrete variable counts
 		start = perf_counter()
 		for label in [0, 1]:
@@ -29,11 +29,11 @@ class NBClassifier:
 			self.sigma[label] = subset.std(numeric_only=True)
 			self.gender_llh[label]['F'] = len(subset[subset['gender'] == 'F']) / len(subset)
 			self.gender_llh[label]['M'] = len(subset[subset['gender'] == 'M']) / len(subset)
-			self.label_counts[label] = len(subset)
+			# self.label_counts[label] = len(subset)
 			
-		self.train_size = len(df)
 		stop = perf_counter()
-		print(f"trained classifier in {stop - start:0.3f} seconds")
+		if print_time:
+			print(f"trained classifier in {stop - start:0.3f} seconds")
 
 	def normpdf(self, col, mean, std):
 		return (
@@ -42,7 +42,7 @@ class NBClassifier:
 			) / (std * root2pi)
 		)
 	
-	def test(self, df, params, print_pred=False):
+	def test(self, df, params, print_pred=False, report=True):
 		start = perf_counter()
 		df['p0'] = 0
 		df['p1'] = 0
@@ -82,9 +82,10 @@ class NBClassifier:
 		correct = (pred == df['label']).sum()
 
 		accuracy = correct / len(df)
-		print(accuracy)
-		stop = perf_counter()
-		print(f"ran inference in {stop - start:0.3f} seconds")
+		if report:
+			print(f"accuracy: {accuracy}")
+			stop = perf_counter()
+			print(f"ran inference in {stop - start:0.3f} seconds")
 		return accuracy
 
 header = [
@@ -93,38 +94,35 @@ header = [
 	'sit_and_bend_forward_cm', 'sit_up_count', 'broad_jump_cm', 'label'
 ]
 
-params = [
+all_params = [
 	'age', 'gender', 'height_cm', 'weight_kg', 
 	'body_fat_pct', 'diastolic', 'systolic', 'grip_force', 
 	'sit_and_bend_forward_cm', 'sit_up_count', 'broad_jump_cm'
 ]
 
+my_params = ['gender', 'weight_kg', 'diastolic', 'grip_force', 'sit_and_bend_forward_cm', 'sit_up_count']
 
-train_fname = 'train.txt'
-test_fname = 'test.txt'
-print('header:', header)
-train_df = pd.read_csv(train_fname, names=header)
-test_df = pd.read_csv(test_fname, names=header)
+if __name__ == "__main__":
+	# train_fname = 'train.txt'
+	# test_fname = 'test.txt'
+	# print('header:', header)
 
-nb = NBClassifier()
-nb.train(train_df)
-nb.test(test_df, params)
+	if len(sys.argv) < 3:
+		sys.exit('please supply 2 command line arguments: [train filename] [test filename]')
 
-nb.test(test_df, ['gender'])
+	train_fname = sys.argv[1]
+	test_fname = sys.argv[2]
+	train_df = pd.read_csv(train_fname, names=header)
+	test_df = pd.read_csv(test_fname, names=header)
 
-# experiment 1: try dropping single parameters
-def experiment1():
-	top_score = 0
-	top_params = []
-	for param in params:
-		temp = params.copy()
-		temp.remove(param)
-		score = nb.test(test_df, temp)
-		if score > top_score:
-			top_score = score
-			top_params = temp
-	print('--------\nThe Best:')
-	print(top_score)
-	print(top_params)
+	nb = NBClassifier()
+	nb.train(train_df, print_time=False)
 
-# experiment1()
+	# personal testing vs gradescope submission
+	if len(sys.argv) == 3:
+		nb.test(test_df, my_params, print_pred=True, report=False)
+	else:
+		nb.test(test_df, my_params, print_pred=False, report=True)
+
+	# top10 = test_df[:10]
+	# nb.test(top10, params, print_pred=True, report=False)
--- prep.py
+++ prep.py
@@ -6,3 +6,9 @@ header = [
 	'body_fat_pct', 'diastolic', 'systolic', 'grip_force', 
 	'sit_and_bend_forward_cm', 'sit_up_count', 'broad_jump_cm', 'label'
 ]
+
+train_fname = 'train.txt'
+test_fname = 'test.txt'
+print('header:', header)
+train_df = pd.read_csv(train_fname, names=header)
+test_df = pd.read_csv(test_fname, names=header)
