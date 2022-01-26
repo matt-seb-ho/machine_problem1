4997d5bf8d5e18411c86aeb9ecc46f6187f27902
Matthew Ho <msho@ucsb.edu>
2022-01-25 19:06:19 -0800

    speedy boi

--- nb_classifier.py
+++ nb_classifier.py
@@ -3,14 +3,18 @@ import pandas as pd
 from scipy.stats import norm
 from time import perf_counter
 
+# constant for normpdf
+root2pi = np.sqrt(2 * np.pi)
+
 class NBClassifier:
 	def __init__(self):
 		# gaussian distribution parameters for continuous variables, indexed on label
 		self.mu = [None, None]
 		self.sigma = [None, None]
 
-		# gender_counts[l][g] = # of training points with label l and gender g
-		self.gender_counts = [{}, {}]
+		# gender likelihoods
+		# gender_llh[l][g] = P(gender=g | label=l) = #(g, l) / #(l)
+		self.gender_llh = [{}, {}]
 
 		# label_counts 
 		self.label_counts = [None, None]
@@ -23,12 +27,20 @@ class NBClassifier:
 			subset = df[df['label'] == label]
 			self.mu[label] = subset.mean(numeric_only=True)
 			self.sigma[label] = subset.std(numeric_only=True)
-			self.gender_counts[label]['F'] = len(subset[subset['gender'] == 'F'])
-			self.gender_counts[label]['M'] = len(subset) - self.gender_counts[label]['F']
+			self.gender_llh[label]['F'] = len(subset[subset['gender'] == 'F']) / len(subset)
+			self.gender_llh[label]['M'] = len(subset[subset['gender'] == 'M']) / len(subset)
 			self.label_counts[label] = len(subset)
+			
 		self.train_size = len(df)
 		stop = perf_counter()
 		print(f"trained classifier in {stop - start:0.3f} seconds")
+
+	def normpdf(self, col, mean, std):
+		return (
+			np.exp(
+				-0.5 * (((col - mean) / std) ** 2)
+			) / (std * root2pi)
+		)
 	
 	def test(self, df, params, print_pred=False):
 		start = perf_counter()
@@ -36,30 +48,44 @@ class NBClassifier:
 		df['p1'] = 0
 		for param in params:
 			if param == 'gender':
-				df['p0'] += np.log2(df['gender'].apply(
-					lambda x : self.gender_counts[0][x] / self.label_counts[0]
-				))
-				df['p1'] += np.log2(df['gender'].apply(
-					lambda x : self.gender_counts[1][x] / self.label_counts[1]
-				))
+				# df['p0'] += np.log2(df['gender'].apply(
+				# 	lambda x : self.gender_counts[0][x] / self.label_counts[0]
+				# ))
+				# df['p1'] += np.log2(df['gender'].apply(
+				# 	lambda x : self.gender_counts[1][x] / self.label_counts[1]
+				# ))
+				
+				df['p0'] = np.log2(df['gender'].map(self.gender_llh[0]))
+				df['p1'] = np.log2(df['gender'].map(self.gender_llh[1]))
+
+
 			else:
-				gauss0 = norm(self.mu[0][param], self.sigma[0][param])
-				gauss1 = norm(self.mu[1][param], self.sigma[1][param])
-				df['p0'] += np.log2(df[param].apply(lambda x : gauss0.pdf(x)))
-				df['p1'] += np.log2(df[param].apply(lambda x : gauss1.pdf(x)))
+				# gauss0 = norm(self.mu[0][param], self.sigma[0][param])
+				# gauss1 = norm(self.mu[1][param], self.sigma[1][param])
+				# df['p0'] += np.log2(df[param].apply(lambda x : gauss0.pdf(x)))
+				# df['p1'] += np.log2(df[param].apply(lambda x : gauss1.pdf(x)))
+
+				df['p0'] += np.log2(self.normpdf(df[param], self.mu[0][param], self.sigma[0][param]))
+				df['p1'] += np.log2(self.normpdf(df[param], self.mu[1][param], self.sigma[1][param]))
+
 		correct = 0
-		for idx, row in df.iterrows():
-			guess = 0 if row['p0'] > row['p1'] else 1
-			if guess == row['label']:
-				correct += 1
-				if print_pred:
-					print(guess)
+		# for idx, row in df.iterrows():
+		# 	guess = 0 if row['p0'] > row['p1'] else 1
+		# 	if guess == row['label']:
+		# 		correct += 1
+		# 		if print_pred:
+		# 			print(guess)
+		
+		pred = df['p1'] > df['p0'] 
+		if print_pred:
+			print(pred.astype('i1').to_string(index=False))
+		correct = (pred == df['label']).sum()
+
 		accuracy = correct / len(df)
 		print(accuracy)
 		stop = perf_counter()
 		print(f"ran inference in {stop - start:0.3f} seconds")
 		return accuracy
-
 
 header = [
 	'age', 'gender', 'height_cm', 'weight_kg', 
@@ -76,12 +102,15 @@ params = [
 
 train_fname = 'train.txt'
 test_fname = 'test.txt'
+print('header:', header)
 train_df = pd.read_csv(train_fname, names=header)
 test_df = pd.read_csv(test_fname, names=header)
 
 nb = NBClassifier()
 nb.train(train_df)
 nb.test(test_df, params)
+
+nb.test(test_df, ['gender'])
 
 # experiment 1: try dropping single parameters
 def experiment1():
@@ -93,7 +122,7 @@ def experiment1():
 		score = nb.test(test_df, temp)
 		if score > top_score:
 			top_score = score
-			top_params = params
+			top_params = temp
 	print('--------\nThe Best:')
 	print(top_score)
 	print(top_params)
