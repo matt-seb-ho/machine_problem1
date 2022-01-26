12ad154c1ddea44a76fd1e0256237f8ddb730cf3
Matthew Ho <msho@ucsb.edu>
2022-01-25 23:08:44 -0800

    starter weights

--- prep.py
+++ prep.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import nb_classifier as nb_mod
 
 header = [
 	'age', 'gender', 'height_cm', 'weight_kg', 
@@ -12,3 +13,6 @@ test_fname = 'test.txt'
 print('header:', header)
 train_df = pd.read_csv(train_fname, names=header)
 test_df = pd.read_csv(test_fname, names=header)
+
+nb = nb_mod.NBClassifier()
+nb.train(train_df, print_time=False)
