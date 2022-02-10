843519a2e99c16cdfb4b603e06d63162ae19ef9c
Matthew Ho <msho@ucsb.edu>
2022-02-10 02:50:53 -0800

    added random exploration to stuck grid search

--- grid.py
+++ grid.py
@@ -1,8 +1,8 @@
 from tqdm import tqdm
 import numpy as np
 
-def code_to_diff(n, params, scale):
-    res = np.zeros(len(params))
+def code_to_diff(n, num_params, scale):
+    res = np.zeros(num_params)
     idx = 0
     while n > 0:
         if n & 1:
@@ -14,25 +14,35 @@ def code_to_diff(n, params, scale):
     res *= scale
     return res
 
-print('grid sig: def grid(nb, df, params, weights, scale, disp=True):')
 
-def grid(nb, df, params, weights, scale, disp=True):
+def make_step_mat(num_params, save_file=None):
+    lst = []
+    for n in range(4 ** num_params):
+        diff = code_to_diff(n, num_params, 1)
+        if diff is not None:
+            lst.append(diff)
+    mat = np.stack(lst)
+    print('^-^)b done!')
+    if save_file is not None:
+        np.save(save_file, mat)
+    return mat
+
+sm6d = np.load("grid_step_6d.npy")
+print('grid sig: def grid(nb, df, params, weights, scale, disp=True):')
+def grid(nb, df, params, weights, scale, sm=sm6d, disp=True):
     init_score = nb.test(df, params, weights, report=False)
     top_score = init_score
     top_weights = np.copy(weights)
    
-    lst = range(4 ** len(params))
+    lst = sm6d * scale
     if disp:
         lst = tqdm(lst)
 
-    for i in lst:
-        diff = code_to_diff(i, params, scale)
-        if diff is None:
-            continue
-        score = nb.test(df, params, weights + diff, report=False)
+    for step in lst:
+        score = nb.test(df, params, weights + step, report=False)
         if score > top_score:
             top_score = score
-            top_weights = np.copy(weights + diff)
+            top_weights = np.copy(weights + step)
     if disp:
         print(f'score after: {top_score}')
         if init_score == top_score:
@@ -45,15 +55,54 @@ def iterated_gs(nb, df, params, weights, scale=.1, max_iter=100, save_file=None,
     score = nb.test(df, params, weights, report=False)
     improving = True
     iters = 0
-    desperate = [0.05, 0.2, 0.3, 0.5, 1]
+    desperate = [0.05, 0.02, 0.01, 0.005, 0.001]
     while improving and iters < max_iter:
         score, weights, improving = grid(nb, df, params, weights, scale, disp)
+        iters += 1
         if not improving:
             for pls in desperate:
                 score, weights, improving = grid(nb, df, params, weights, pls, disp)
+                iters += 1
                 if improving:
                     break
-        iters += 1
     print(score)
     if save_file is not None:
         np.save(save_file, weights)
+
+def random_unit_vec(dim):
+	vec = np.random.rand(dim) - 0.5
+	length = np.linalg.norm(vec)
+	return vec / length
+
+print('stochastic sig: def hybrid_search(nb, df, ps, weights, scale=.1, max_steps=200, save_file="weights/dummy.npy", bar=True):')
+milestones = { round(0.01 * pct, 2) : False for pct in range(82, 89) }
+decayed_steps = [0.05, 0.01, 0.001]
+def hybrid_search(
+        nb, df, ps, weights, 
+        scale=.1, max_steps=200, save_every=1e10,
+        save_file="weights/dummy", bar=True):
+    score = nb.test(df, ps, weights, report=False)
+    improving = True
+    milestone = .82
+    looper = tqdm(range(max_steps)) if bar else range(max_steps)
+    for idx in looper:
+        # if we haven't reached this milestone yet
+        if score > milestone:
+            np.save(save_file + f'_m{milestone}.npy', weights)
+            milestones[milestone] = True
+            milestone += 0.01
+            milestones[milestone] = False
+
+        if idx and idx % save_every == 0:
+            np.save(save_file + str(idx) + ".npy", weights)
+        score, weights, improving = grid(nb, df, ps, weights, scale, disp=False)
+        if not improving:
+            for decayed in decayed_steps:
+                score, weights, improving = grid(nb, df, ps, weights, scale, disp=False)
+                if improving:
+                    break
+        if not improving:
+            weights += 2 * scale * (random_unit_vec(len(ps)))
+    print(f'final score: {score}')
+    np.save(save_file + ".npy", weights)
+                
